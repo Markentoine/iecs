bbb70821eed5752f38fef70f40805c354f59265f
Markentoine <monsieur.toulouse@gmail.com>
2019-05-18 12:09:29 +0200

    Fade out subtitle.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 'use strict'
-
 const rellax = new Rellax('.rellax');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const subtitle = document.getElementById('subtitle');
+    const fadeOUtSubtitle = e => {
+        const unitOpacity = 1 / 500;
+        const opa = unitOpacity * window.scrollY;
+        console.log(opa)
+        if (opa >= 1) {
+            subtitle.style.opacity = 0;
+        } else {
+            subtitle.style.opacity = `${1 - opa}`;
+        }
+    }
+    document.onscroll = fadeOUtSubtitle;
+
+})
