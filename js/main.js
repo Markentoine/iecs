c96157aaff2c8707833b2aa3c67cb110a3198b2c
Markentoine <monsieur.toulouse@gmail.com>
2019-05-24 15:18:05 +0200

    WIP.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@
 const rellax = new Rellax('.rellax');
 
 document.addEventListener('DOMContentLoaded', () => {
+    const windowWidth = window.innerWidth;
     const subtitle = document.getElementById('subtitle');
+    
+    if (windowWidth <= 800) {
+        const contactMail = document.getElementById('iecsFR');
+        contactMail.remove();
+    }
     const fadeOUtSubtitle = e => {
         const unitOpacity = 1 / 500;
         const opa = unitOpacity * window.scrollY;
