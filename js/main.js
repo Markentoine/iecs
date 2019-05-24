1f063623f412429a02c1eddaf0de587688b17f2c
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-05-24 20:57:28 +0200

    WIP.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const rellax = new Rellax('.rellax');
+let rellax = new Rellax('.rellax');
 
 document.addEventListener('DOMContentLoaded', () => {
     const windowWidth = window.innerWidth;
@@ -13,11 +13,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const contactMail = document.getElementById('iecsFR');
         contactMail.remove();
 
+        // change svg icons size
         const icons = document.getElementsByClassName('icon-clef');
         [...icons].map(e => e.firstElementChild).forEach(e => {
             e.setAttribute('width', 72);
             e.setAttribute('height', 72);
         });
+
+        //remove rellax effect
+        const rellaxedEls = document.getElementsByClassName('rellax');
+        [...rellaxedEls].forEach(el => {
+            el.classList.remove('rellax');
+            el.dataset.rellaxSpeed = ""
+        });
+        rellax = null;
     }
 
     logoHead.onclick = () => window.scroll(0, 0);
