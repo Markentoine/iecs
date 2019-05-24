f9eaa884b0133f6b1e1fa5bd94694834bb2dae1d
Markentoine <monsieur.toulouse@gmail.com>
2019-05-24 15:28:38 +0200

    Window scroll back on logo-head click.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 'use strict'
+
 const rellax = new Rellax('.rellax');
 
 document.addEventListener('DOMContentLoaded', () => {
     const windowWidth = window.innerWidth;
     const subtitle = document.getElementById('subtitle');
-    
+    const logoHead = document.getElementById("logo-head");
+
+    // behavior for mobile devices ----------------------
+
     if (windowWidth <= 800) {
         const contactMail = document.getElementById('iecsFR');
         contactMail.remove();
     }
-    const fadeOUtSubtitle = e => {
-        const unitOpacity = 1 / 500;
-        const opa = unitOpacity * window.scrollY;
-        console.log(opa)
-        if (opa >= 1) {
-            subtitle.style.opacity = 0;
-        } else {
-            subtitle.style.opacity = `${1 - opa}`;
-        }
-    }
-    document.onscroll = fadeOUtSubtitle;
+
+    logoHead.onclick = () => window.scroll(0, 0);
+
+    // --------------------------------------------------
+
+    const effects = {
+        fadeOUtSubtitle: e => {
+            const unitOpacity = 1 / 500;
+            const opa = unitOpacity * window.scrollY;
+            if (opa >= 1) {
+                subtitle.style.opacity = 0;
+            } else {
+                subtitle.style.opacity = `${1 - opa}`;
+            }
+        },
+
+    };
+
+
+
+    document.onscroll = effects.fadeOUtSubtitle;
 })
